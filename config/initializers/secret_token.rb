4cf361a5158b1ebe12eba654b1b972cfc5354c1b
maxaxis <lockout51@gmail.com>
2012-04-06 23:28:07 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aditya::Application.config.secret_token = '1c643c236df7d83d46a415f459cc496c9c5d6d558d465aff546d1521e1e6dbac0720072716d316f28c8c120ddc63560e4277ec961f3360de8bc47f07277051ca'
